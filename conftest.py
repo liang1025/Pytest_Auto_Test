8ec458c92700f37fec2d3ff23c54974910cf34c7
Liang <hong.liang@qq.com>
2020-08-12 23:14:26 +0800

    去掉截图功能

--- conftest.py
+++ conftest.py
@@ -19,8 +19,8 @@ import time
 
 @pytest.fixture(scope='function')
 def quit_driver():
-    log.info("conftest文件初始化")
-    cf.init()
+    # log.info("conftest文件初始化")
+    # cf.init()
     binary_location = '/usr/bin/google-chrome'
     chrome_driver_binary = '/usr/bin/chromedriver'
     chrome_options = webdriver.ChromeOptions()
@@ -73,32 +73,32 @@ def pytest_collection_modifyitems(items):
         item.name = item.name.encode("utf-8").decode("unicode_escape")
         item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
 
-@pytest.mark.hookwrapper
-def pytest_runtest_makereport(item):
-    """当测试失败的时候，自动截图，展示到html报告中"""
-    pytest_html = item.config.pluginmanager.getplugin('html')
-    outcome = yield
-    report = outcome.get_result()
-    extra = getattr(report, 'extra', [])
-
-    if report.when == 'call' or report.when == "setup":
-        xfail = hasattr(report, 'wasxfail')
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            file_name = report.nodeid.replace("::", "_")+".png"
-            screen_img = _capture_screenshot()
-            if file_name:
-                html = '<div><img src="data:image/png;base64,%s" alt="screenshot" style="width:600px;height:250px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % screen_img
-                extra.append(pytest_html.extras.html(html))
-        report.extra = extra
-        report.description = str(item.function.__doc__)
-        report.module = str(item.module.__doc__)
-        # report.nodeid = report.nodeid.encode("utf-8").decode("unicode_escape")  # 解决乱码
-
-
-def _capture_screenshot():
-    '''截图保存为base64'''
-    return cf.get_value('driver').get_screenshot_as_base64()
+# @pytest.mark.hookwrapper
+# def pytest_runtest_makereport(item):
+#     """当测试失败的时候，自动截图，展示到html报告中"""
+#     pytest_html = item.config.pluginmanager.getplugin('html')
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, 'extra', [])
+#
+#     if report.when == 'call' or report.when == "setup":
+#         xfail = hasattr(report, 'wasxfail')
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             file_name = report.nodeid.replace("::", "_")+".png"
+#             screen_img = _capture_screenshot()
+#             if file_name:
+#                 html = '<div><img src="data:image/png;base64,%s" alt="screenshot" style="width:600px;height:250px;" ' \
+#                        'onclick="window.open(this.src)" align="right"/></div>' % screen_img
+#                 extra.append(pytest_html.extras.html(html))
+#         report.extra = extra
+#         report.description = str(item.function.__doc__)
+#         report.module = str(item.module.__doc__)
+#         # report.nodeid = report.nodeid.encode("utf-8").decode("unicode_escape")  # 解决乱码
+#
+#
+# def _capture_screenshot():
+#     '''截图保存为base64'''
+#     return cf.get_value('driver').get_screenshot_as_base64()
 
 
 @pytest.hookimpl(optionalhook=True)
