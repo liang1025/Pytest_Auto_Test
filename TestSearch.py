92c4d34c7d925d2bf19dacf9a0b6965f4b8535b4
Liang <hong.liang@qq.com>
2020-08-16 18:48:00 +0800

    修改TestSearch文件

--- TestSearch.py
+++ TestSearch.py
@@ -11,9 +11,9 @@
 @desc:
 '''
 
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+# import os
+# import sys
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 import pytest
 from pages.SearchPage import SearchPage
