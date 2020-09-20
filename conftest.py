29a9c577073f977453de99b87f999c932245b103
Liang <hong.liang@qq.com>
2020-09-20 13:05:14 +0800

    allure增加截图

--- conftest.py
+++ conftest.py
@@ -13,6 +13,7 @@ import config.config as cf
 from common.mainModule import log
 from py._xmlgen import html
 import time
+import allure
 from selenium.webdriver.chrome.options import Options
 
 
@@ -55,6 +56,9 @@ def pytest_collection_modifyitems(items):
         item.name = item.name.encode("utf-8").decode("unicode_escape")
         item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
 
+
+'''
+# 使用pytest-html报告，使用该方法
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item):
     """当测试失败的时候，自动截图，展示到html报告中"""
@@ -79,8 +83,36 @@ def pytest_runtest_makereport(item):
 
 
 def _capture_screenshot():
-    '''截图保存为base64'''
+    #截图保存为base64
     return cf.get_value('driver').get_screenshot_as_base64()
+'''
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    '''
+    获取每个用例状态的钩子函数
+    :param item:
+    :param call:
+    :return:
+    '''
+    # 获取钩子方法的调用结果
+    outcome = yield
+    rep = outcome.get_result()
+    # 仅仅获取用例call 执行结果是失败的情况, 不包含 setup/teardown
+    if rep.when == "call" and rep.failed:
+        mode = "a" if os.path.exists("failures") else "w"
+        with open("failures", mode) as f:
+            # let's also access a fixture for the fun of it
+            if "tmpdir" in item.fixturenames:
+                extra = " (%s)" % item.funcargs["tmpdir"]
+            else:
+                extra = ""
+            f.write(rep.nodeid + extra + "\n")
+        # 添加allure报告截图
+        if hasattr(cf.get_value('driver'), "get_screenshot_as_png"):
+            with allure.step('添加失败截图...'):
+                allure.attach(cf.get_value('driver').get_screenshot_as_png(), "失败截图", allure.attachment_type.PNG)
 
 
 @pytest.hookimpl(optionalhook=True)
